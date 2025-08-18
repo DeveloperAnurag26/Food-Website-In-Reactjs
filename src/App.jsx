44dafacd0a4d61d6dc4af20011914249f97d1fe5
Anurag Pandey <anuragpandey537755@gmail.com>
2025-08-18 21:35:10 +0530

    added Login Popup Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Pages/Home/Home'
 import Cart from './Components/Pages/Home/Cart/Cart'
@@ -6,12 +6,16 @@ import PlaceOrder from './Components/Pages/PlaceOrder/PlaceOrder'
 
 import { Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
+import LoginPopup from './Components/LoginPopup/LoginPopup'
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
-      <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
